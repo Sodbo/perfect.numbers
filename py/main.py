c142747bf16238190389914ed17de304463e76c5
Sodbo Sharapov <sharapovsodbo@gmail.com>
2016-01-25 23:02:41 +0100

    Added multiprocessor feature. Interpretation failed

--- py/main.py
+++ py/main.py
@@ -9,32 +9,32 @@ N = 1000000
 
 output = mp.Queue()
 
-def checkPerfectNumber(num):
+def checkPerfectNumber(num, output):
 	a = 1
 	for num2 in range(2,round(math.sqrt(num))+1):
 		if num % num2 == 0:
 			a = a+num2+num//num2
 	if(a==num):
-		print('Perfect number ',num)
+		output.put(num)
 
-for i in range(1,N):
-	checkPerfectNumber(i)
+#for i in range(1,N):
+#	checkPerfectNumber(i)
 	
-#processes = [mp.Process(target=checkPerfectNumber, args=(10, output)) for x in range(2)]
+processes = [mp.Process(target=checkPerfectNumber, args=(10, output)) for x in range(2)]
 
 # Run processes
-#for p in processes:
-#    p.start()
+for p in processes:
+	p.start()
 
 # Exit the completed processes
-#for p in processes:
-#    p.join()
+for p in processes:
+    p.join()
 
 # Get process results from the output queue
-#results = [output.get() for p in processes]
+results = [output.get() for p in processes]
 
 stop = time.time()
 
-#print(results)
+print(results)
 
 print(stop-start)
