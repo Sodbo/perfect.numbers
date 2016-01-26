f437628206818d84f59a237a31fe1b9fa9505697
sodbo <sodbo@sodbo.hzm.pc>
2016-01-26 17:10:00 +0100

    working parallel version. Performance boost by switching to 1000x chunks

--- py/main.py
+++ py/main.py
@@ -5,53 +5,26 @@ import math
 import string
 import Queue
 
-N=1000000
-
-def dump_queue(queue):
-    result = []
-    for i in iter(queue.get, 'STOP'):
-        result.append(i)
-    time.sleep(.1)
-    return result
+N=34000000
+# Runtime 8 seconds
 
 start = time.time()
 
-myoutput = mp.Queue()
+def checkPerfectNumber(numlow,numup):
+	result = []
+	for num in range(numlow,numup+1):
+		a = 1
+		for num2 in range(2,int(round(math.sqrt(num)))+1):
+			if num % num2 == 0:
+				a = a+num2+num//num2
+		if(a==num):
+			result = result + [num]
+	return result
 
-def checkPerfectNumber(num):
-	a = 1
-	for num2 in range(2,int(round(math.sqrt(num)))+1):
-		if num % num2 == 0:
-			a = a+num2+num//num2
-	if(a==num):
-		return(num)
-	else:
-		return
-
-#processes = [mp.Process(target=checkPerfectNumber, args=(1000000,myoutput,)) for x in range(4)]
-
-# Run processes
-#for p in processes:
-#	p.start()
-
-# Exit the completed processes
-#for p in processes:
-#    p.join()
-
-#myoutput.put('STOP')
-
-# Get process results from the output queue
-
-#results = dump_queue(myoutput)
-#print(results)
-
-#stop = time.time()
-
-#print(results)
-
-#print(stop-start)
-
-pool = mp.Pool(processes=5)
-results = [pool.apply_async(checkPerfectNumber, args=(x,)) for x in range(2,N)]
+pool = mp.Pool(processes=4)
+results = [pool.apply_async(checkPerfectNumber, args=(x*1000+1,x*1000+1000)) for x in range(0,int(N/1000))]
 output = [p.get() for p in results]
 print(output)
+
+stop = time.time()
+print(stop-start)
