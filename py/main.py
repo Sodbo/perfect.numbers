ada514fa67db1fee76bbedcfce60c1e24bc5d606
sodbo <sodbo@sodbo.hzm.pc>
2016-01-26 15:45:09 +0100

    Working parallel version but with a lot of None in the output

--- py/main.py
+++ py/main.py
@@ -5,6 +5,8 @@ import math
 import string
 import Queue
 
+N=1000000
+
 def dump_queue(queue):
     result = []
     for i in iter(queue.get, 'STOP'):
@@ -16,33 +18,40 @@ start = time.time()
 
 myoutput = mp.Queue()
 
-def checkPerfectNumber(num, output):
+def checkPerfectNumber(num):
 	a = 1
 	for num2 in range(2,int(round(math.sqrt(num)))+1):
 		if num % num2 == 0:
 			a = a+num2+num//num2
 	if(a==num):
-		output.put(num)
+		return(num)
+	else:
+		return
 
-processes = [mp.Process(target=checkPerfectNumber, args=(1000000,myoutput,)) for x in range(4)]
+#processes = [mp.Process(target=checkPerfectNumber, args=(1000000,myoutput,)) for x in range(4)]
 
 # Run processes
-for p in processes:
-	p.start()
+#for p in processes:
+#	p.start()
 
 # Exit the completed processes
-for p in processes:
-    p.join()
+#for p in processes:
+#    p.join()
 
-myoutput.put('STOP')
+#myoutput.put('STOP')
 
 # Get process results from the output queue
 
-results = dump_queue(myoutput)
-print(results)
+#results = dump_queue(myoutput)
+#print(results)
 
 #stop = time.time()
 
 #print(results)
 
 #print(stop-start)
+
+pool = mp.Pool(processes=5)
+results = [pool.apply_async(checkPerfectNumber, args=(x,)) for x in range(2,N)]
+output = [p.get() for p in results]
+print(output)
