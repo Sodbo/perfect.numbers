b82d1501540efe1cf4ebd239c536aa51b3a74e66
sodbo <sodbo@sodbo.hzm.pc>
2016-01-26 15:23:27 +0100

    Do not know where is bug

--- py/main.py
+++ py/main.py
@@ -2,23 +2,29 @@
 import multiprocessing as mp
 import time
 import math
+import string
+import Queue
+
+def dump_queue(queue):
+    result = []
+    for i in iter(queue.get, 'STOP'):
+        result.append(i)
+    time.sleep(.1)
+    return result
 
 start = time.time()
 
-output = mp.Queue()
+myoutput = mp.Queue()
 
 def checkPerfectNumber(num, output):
 	a = 1
-	for num2 in range(2,round(math.sqrt(num))+1):
+	for num2 in range(2,int(round(math.sqrt(num)))+1):
 		if num % num2 == 0:
 			a = a+num2+num//num2
 	if(a==num):
 		output.put(num)
 
-#for i in range(1,N):
-#	checkPerfectNumber(i)
-	
-processes = [mp.Process(target=checkPerfectNumber, args=(10, output)) for x in range(2)]
+processes = [mp.Process(target=checkPerfectNumber, args=(1000000,myoutput,)) for x in range(4)]
 
 # Run processes
 for p in processes:
@@ -28,11 +34,15 @@ for p in processes:
 for p in processes:
     p.join()
 
+myoutput.put('STOP')
+
 # Get process results from the output queue
-results = [output.get() for p in processes]
 
-stop = time.time()
-
+results = dump_queue(myoutput)
 print(results)
 
-print(stop-start)
+#stop = time.time()
+
+#print(results)
+
+#print(stop-start)
