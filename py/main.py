4cb9fcf78661962ca8046c6771baed2a006cc1dd
sodbo <sodbo@sodbo.hzm.pc>
2016-01-26 14:20:39 +0100

    Switched to python2.7 version

--- py/main.py
+++ py/main.py
@@ -3,13 +3,12 @@ import time
 import math
 
 N = 1000000
-# Runtime 70.23607802391052
+# Runtime 47.429256916
 
->>>>>>> master
 start = time.time()
 for num in range(2,N+1):
 	a = 1
-	for num2 in range(2,round(math.sqrt(num))+1):
+	for num2 in range(2,int(round(math.sqrt(num))+1)):
 		if num % num2 == 0:
 			a = a+num2+num//num2
 	if(a==num):
