0b1edcdf8bb637e6ec240ba2e790b28b5bfd0408
Sodbo Sharapov <sharapovsodbo@gmail.com>
2016-01-29 00:01:56 +0100

    added base.line cplusplus algotirhm to base.algorithm branch

--- cplus/main.cpp
+++ cplus/main.cpp
@@ -1,7 +1,21 @@
 # include <iostream>
+# include <math.h>
 
-int main(){
-	int N = 10000;
-	std::cout << "Hello, world!\n";
+int main(int argc, char* argv[]){
+
+	int N = 2000000;
+	std::cout << "Starting ride!\n\n";
+	for (int i=2; i<N+1; i++){
+		int a = 1;
+		for (int del=2; del<round(sqrt(i))+1;del++){
+			if (i%del == 0){
+				a = a + del + i/del;
+			}
+		}
+
+		if (a == i)
+			std::cout << "Perfect number " << a << '\n';
+	}
+	std::cout << '\n';
 	return(0);
 }
